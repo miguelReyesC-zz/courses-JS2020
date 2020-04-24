15c5a510831ffa6117d6815fd939cb74e298a2f1
Miguel Reyes <miguel_reyes_c@hotmail.com>
2020-04-23 22:44:07 -0500

    fix

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -3,6 +3,6 @@ let currentResult = defaultResult;
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-let calculatoinDescription = `${defaultResult} + + 10) * 3 /2 -1`;
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
 
-outputResult(currentResult, "");
+outputResult(currentResult, calculationDescription);

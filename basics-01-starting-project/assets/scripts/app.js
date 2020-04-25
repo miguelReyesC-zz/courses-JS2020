86f33730e3484dbd170d50b318039ab31104b09f
Miguel Reyes <miguel_reyes_c@hotmail.com>
2020-04-24 20:32:53 -0500

    splitting code into functions

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,9 +1,39 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+    return +userInput.value;
+}
+
 function add() {
-    currentResult = currentResult + +(userInput.value);
-    outputResult(currentResult, "");
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+function substract() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} - ${enteredNumber}`;
+    currentResult = currentResult - enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} * ${enteredNumber}`;
+    currentResult = currentResult * enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} / ${enteredNumber}`;
+    currentResult = currentResult / enteredNumber;
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", substract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
